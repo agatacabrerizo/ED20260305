8afecfcbed0be6901b85047ebe3ef4f64d279f60
agata <agata@Agata>
2026-03-16 18:27:10 +0100

    ExportacionArchivo atributo + constructor

--- src/jcolonia/daw2025/tablasmvc/ExportacionArchivo.java
+++ src/jcolonia/daw2025/tablasmvc/ExportacionArchivo.java
@@ -1,5 +1,18 @@
 package jcolonia.daw2025.tablasmvc;
 
+import java.io.File;
+import java.nio.file.Path;
+
 public class ExportacionArchivo {
 
+	private Path refArchivo;
+	/**
+     * Constructor. Recibe un texto con la ruta y lo convierte en un objeto Path.
+     * @param rutaArchivo Nombre o ruta del archivo (ejemplo: "mitabla.txt")
+     */
+	public ExportacionArchivo(String rutaArchivo) {
+        File archivo = new File(rutaArchivo);
+        this.refArchivo = archivo.toPath();
+    }
 }
+
