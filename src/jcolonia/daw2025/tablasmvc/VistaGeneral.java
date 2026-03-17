2bc25ac1baf040700b7c71ca3a04281070d1bb99
Ágata Cabrerizo Moreno <agatacabrerizo@gmail.com>
2026-03-17 15:09:35 +0100

    VistaGeneral constantes + métodos vacíos

--- src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
+++ src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
@@ -1,5 +1,22 @@
 package jcolonia.daw2025.tablasmvc;
 
 public class VistaGeneral {
+	
+	public class VistaGeneral {
+
+	    
+	    private static final String FORMATO_PRINTF_TEXTO = "";
+	    private static final String FORMATO_PRINTF_AVISO = "";
+	}
+
+	public static void mostrarAviso(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static int pedirNúmero(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
