8b71057929adb78f4a96f7a8db6a66e288515b1e
Ágata Cabrerizo Moreno <agatacabrerizo@gmail.com>
2026-03-17 15:16:39 +0100

    Importación + implementacion de scanner + métodos interactivos con el usuario

--- src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
+++ src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
@@ -1,9 +1,34 @@
 package jcolonia.daw2025.tablasmvc;
 
+import java.util.Scanner;
+
 public class VistaGeneral {
 
 	    private static final String FORMATO_PRINTF_TEXTO = "";
 	    private static final String FORMATO_PRINTF_AVISO = "";
+	    
+	    private static final Scanner scan = new Scanner(System.in);
+	    
+	    public static int pedirNumero(String texto) {
+	        System.out.print(texto);
+	        while (!scan.hasNextInt()) {
+	            mostrarAviso("Error: Debes introducir un número entero.");
+	            scan.next(); 
+	            System.out.print(texto);
+	        }
+	        
+	        int numero = scan.nextInt();
+	        scan.nextLine(); 
+	        return numero;
+	    }
+	    public static void pausa(String texto) {
+	        System.out.print(texto + " (Pulsa Intro para continuar...)");
+	        scan.nextLine();
+	    }
+	    public static void pedirConfirmacion(String texto) {
+	        System.out.print(texto + " (s/n): ");
+	        scan.nextLine(); 
+	    }
 	    
 	    public static void mostrarTexto(String texto) {
 	        System.out.printf(FORMATO_PRINTF_TEXTO, texto);
